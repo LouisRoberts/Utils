dddf12f225e20967ba57b6b8ee3c5dde9a409f30
LouisRoberts <louisjroberts@gmail.com>
2016-05-06 16:32:03 +0100

    Checking Update
    Add comment

--- src/org/lorob/utils/BaselineVersion.java
+++ src/org/lorob/utils/BaselineVersion.java
@@ -1,5 +1,10 @@
 package org.lorob.utils;
 
+/** Add a comment **
+ * 
+ * @author Louis
+ *
+ */
 public interface BaselineVersion
 {
 	public final static String BASELINE_VERSION="(Version 2.3.0)";
